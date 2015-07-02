a1b6388fabd0de51a02c567953826344ef05890c
Jordan Harband <ljharb@gmail.com>
2015-07-02 01:10:56 -0700

    Update `eslint`, `nsp`

--- test.js
+++ test.js
@@ -5,19 +5,27 @@ var isBoolean = require('./');
 var hasSymbols = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
 
 test('not Booleans', function (t) {
-	t.notOk(isBoolean(), 'undefined is not Boolean');
-	t.notOk(isBoolean(null), 'null is not Boolean');
-	t.notOk(isBoolean(0), '0 is not Boolean');
-	t.notOk(isBoolean(Object(42)), 'number object is not Boolean');
-	t.notOk(isBoolean(NaN), 'NaN is not Boolean');
-	t.notOk(isBoolean(Infinity), 'Infinity is not Boolean');
-	t.notOk(isBoolean('foo'), 'string is not Boolean');
-	t.notOk(isBoolean([]), 'array is not Boolean');
-	t.notOk(isBoolean({}), 'object is not Boolean');
-	t.notOk(isBoolean(function () {}), 'function is not Boolean');
-	t.notOk(isBoolean(/a/g), 'regex literal is not Boolean');
-	t.notOk(isBoolean(new RegExp('a', 'g')), 'regex object is not Boolean');
-	t.notOk(isBoolean(new Date()), 'new Date() is not Boolean');
+	t.test('primitives', function (st) {
+		st.notOk(isBoolean(), 'undefined is not Boolean');
+		st.notOk(isBoolean(null), 'null is not Boolean');
+		st.notOk(isBoolean(0), '0 is not Boolean');
+		st.notOk(isBoolean(NaN), 'NaN is not Boolean');
+		st.notOk(isBoolean(Infinity), 'Infinity is not Boolean');
+		st.notOk(isBoolean('foo'), 'string is not Boolean');
+		st.end();
+	});
+
+	t.test('objects', function (st) {
+		st.notOk(isBoolean(Object(42)), 'number object is not Boolean');
+		st.notOk(isBoolean([]), 'array is not Boolean');
+		st.notOk(isBoolean({}), 'object is not Boolean');
+		st.notOk(isBoolean(function () {}), 'function is not Boolean');
+		st.notOk(isBoolean(/a/g), 'regex literal is not Boolean');
+		st.notOk(isBoolean(new RegExp('a', 'g')), 'regex object is not Boolean');
+		st.notOk(isBoolean(new Date()), 'new Date() is not Boolean');
+		st.end();
+	});
+
 	t.end();
 });
 
